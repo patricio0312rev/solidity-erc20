8b7e3a8d94178b0c1b1d94446075a3da77ea87c6
"patricio0312rev" <"review0312oficial@gmail.com">
2021-12-21 14:38:49 -0500

    Agregando métodos booleanos en interface

--- custom_token/ERC20.sol
+++ custom_token/ERC20.sol
@@ -13,6 +13,15 @@ interface IERC20 {
 
     // Devuelve el número de token que el spender podrá gastar en nombre del propietario (owner)
     function allowance(address owner, address spender) external view returns(uint256);
+
+    // Devuelve un valor booleano resultado de la operación indicada
+    function transfer(address recipient, uint256 amount) external returns(bool);
+
+    // Devuelve un valor booleano con el resultado de la operación gasto
+    function approve(address spender, uint256 amount) external returns(bool);
+
+    // Devuelve un valor booleano con el resultado de la operación de paso de una cantidad de tokens usando el método allowance()
+    function transferFrom(address sender, address recipient, uint256 amount) external returns(bool);
 }
 
 contract ERC20Basic is IERC20 {
@@ -28,5 +37,17 @@ contract ERC20Basic is IERC20 {
     function allowance(address owner, address spender) public override view returns(uint256) {
 
         return 0;
+    }
+
+    function transfer(address recipient, uint256 amount) public override returns(bool) {
+        return false;
+    }
+
+    function approve(address spender, uint256 amount) public override returns(bool) {
+        return false;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public override returns(bool){
+        return false;
     }
 }
