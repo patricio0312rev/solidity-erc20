be0df718660f08dba97ddc2c1bd0f96e1f955da7
"patricio0312rev" <"review0312oficial@gmail.com">
2021-12-21 14:26:36 -0500

    Creando metodos en interface

--- custom_token/ERC20.sol
+++ custom_token/ERC20.sol
@@ -2,3 +2,31 @@
 pragma solidity >=0.4.0 < 0.7.0;
 pragma experimental ABIEncoderV2;
 import "./SafeMath.sol";
+
+// Interface de nuestro token ERC20
+interface IERC20 {
+    // Devuelve la cantidad de tokens en existencia
+    function totalSupply() external view returns(uint256);
+
+    // Devuelve la cantidad de tokens para una dirección indicada por parámetro
+    function balanceOf(address account) external view returns(uint256);
+
+    // Devuelve el número de token que el spender podrá gastar en nombre del propietario (owner)
+    function allowance(address owner, address spender) external view returns(uint256);
+}
+
+contract ERC20Basic is IERC20 {
+    function totalSupply() public override view returns(uint256) {
+        return 0;
+    }
+
+    function balanceOf(address account) public override view returns(uint256) {
+
+        return 0;
+    }
+
+    function allowance(address owner, address spender) public override view returns(uint256) {
+
+        return 0;
+    }
+}
